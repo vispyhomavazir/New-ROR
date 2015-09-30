eb378684bbb115b3f690d118f79a12820a8438b2
vispy homavazir <vispy.homavazir@gmail.com>
2015-09-29 22:17:05 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ROR::Application.config.secret_token = '67f398425cfe1de5137c62e52bb1bef40a16612e8e78407c21e5934a2d1d5294e097f992ae201e1c941a5729ae62f14d6e619f8e544f656d1318d6c3c715080f'
